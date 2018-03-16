e06204023dd365cbc9e5dbb168ab6e30ae24515e
root <root@influenza-dev.cvm.iastate.edu>
2018-03-15 19:23:36 -0500

    Changing style and adding references to the identity tool

--- identity.php
+++ identity.php
@@ -8,7 +8,7 @@ $theme->addStyle('{{asset_path}}/css/jquery-ui.css');
 $theme->drawHeader();
 ?>
 
-<h2 id="chartTitle">BLAST Tool</h2>
+<h2 id="chartTitle">Sequence Identity Tool</h2>
 
 <div class="wd-Alert" id="wait">
 Please wait, BLAST in progress...
@@ -16,16 +16,25 @@ Please wait, BLAST in progress...
 <form id="target">
 <textarea rows="16" cols="100" id="sequences" placeholder="Paste sequences (fasta/plain text)">
 </textarea>
-<b>BLAST type</b><br/>
+<b>Sequence type</b><br/>
 <input type="radio" name="blasttype" value="nt" checked="checked" />nucleotide<br>
 <input type="radio" name="blasttype" value="aa" />amino acid<br>
-<a class="wd-Button" id="submit">BLAST</a>
+<a class="wd-Button" id="submit">Search</a>
 </form>
 
 <div id="results">
 
 </div>
-
+<br/>
+<div>
+<small>
+<h3>References</h3>
+<ol>
+<li>Altschul, S.F., Gish, W., Miller, W., Myers, E.W. & Lipman, D.J. (1990) "Basic local alignment search tool." J. Mol. Biol. 215:403-410.</li>
+<li>Zhang Z., Schwartz S., Wagner L., & Miller W. (2000), "A greedy algorithm for aligning DNA sequences" J Comput Biol 2000; 7(1-2):203-14.</li>
+</ol>
+</small>
+</div>
 <script>
 
 $(document).ready(function() {
