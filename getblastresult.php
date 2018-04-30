63792169cab8f1192be04464aa9fbcf4f43effd3
root <root@influenza-dev.cvm.iastate.edu>
2018-04-30 13:59:12 -0500

    Modified BLAST output to 1. Display subtype information 2. Open genabnk windows in new window 3. Correctly handle and filter amino acid results
    TODO
    1. Switch genbank links to IRD
    2. Check/clean database
    	a. Correct states where there are USDA barcodes
    	b. Add subtypes where there are sequences
    	c. Add in full sequence lengths for sequences from before 2017
    3. Correct error message, if there are no results it sayss the input is incorrect rather than there are no results.
    4. Write SOP

--- getblastresult.php
+++ getblastresult.php
@@ -80,6 +80,7 @@ if(count($blastHits) < 2)
 $state = array();
 $haClade = array();
 $naClade = array();
+$subtype = array ();
 
 //Fill table and begin to calculate percentages
 for ($i = 0; $i <= count($blastHits); $i++)
@@ -88,14 +89,20 @@ for ($i = 0; $i <= count($blastHits); $i++)
 	$table .= "<tr>";
 
 	$hits = explode("+", $blastHits[$i]);
-	for ($j = 1; $j < count($hits); $j++)	//This j starts at 1 to leave out db id, less then the count ot leave off blank row
+	for ($j = 1; $j < count($hits); $j++)	//This j starts at 1 to leave out db id, less then the count to leave off blank row
 	{
+		//If the %identity is too low, break assuming results are in order. This is mainly for blastp, which perc_identity does not work
+		if((float)$hits[7] < 96.0)
+		{
+			break;
+		}
+
 		$table .= "<td>"; 
 		
 		//Add in an ncbi link if on the first item
 		if($j == 1 && $hits[$j] != "")
 		{
-			$table .= "<a href=\"https://www.ncbi.nlm.nih.gov/nuccore/?term=" . $hits[$j] . "\">" . $hits[$j] . "</a></td>";
+			$table .= "<a href=\"https://www.ncbi.nlm.nih.gov/nuccore/?term=" . $hits[$j] . "\" target=\"_blank\">" . $hits[$j] . "</a></td>";
 		}
 		elseif($j == 1 && $hits[$j] == "")
 		{
@@ -111,13 +118,15 @@ for ($i = 0; $i <= count($blastHits); $i++)
 			 $hits[$j] = "Not Tested";
 		if($j == 3) //state
 			$state[] = $hits[$j];
+		if($j == 4) //subtype
+			$subtype[] = $hits[$j];
 		if($j == 5)
 			$haClade[] = $hits[$j];
 		if($j == 6)
 			$naClade[] = $hits[$j];
 	}
 
-	//Clsoe row
+	//Close row
 	$table .= "</tr>";
 }
 
@@ -127,17 +136,27 @@ $table .= "</table>";
 $stateProp = array_count_values($state);
 $haClade = array_count_values($haClade);
 $naProp = array_count_values($naClade);
+$subtype = array_count_values($subtype);
 $stateCode = generatePieChart($stateProp,"stateChart");
 $naCode = generatePieChart($naProp,"naChart");
 
 //Get most populous item from array
 $topClade = array_search(max($haClade),$haClade);
+$topSubtype = array_search(max($subtype),$subtype);
+
+//Heuristic; check if left char is an H, if so take 2 chars, else take none
+if($topSubtype[0] == "H")
+{
+	$topSubtype = substr($topSubtype,0,2);
+}
+else
+	$topSubtype = "";
 
 //Send back to server
-echo "<br/><h2>This sequence has the best BLAST match to: <spani style='color:red'>" . $topClade . "</span></h2><br/>";
+echo "<br/><h2>This sequence has the best BLAST match to: <spani style='color:red'>" . $topSubtype . " " . $topClade . "</span></h2><br/>";
 echo <<<EOF
 <div id="wrapper"> 
-        <h2>Influenza cases in ISU FLUture with 98% or greater similarity to query sequence</h2> 
+        <h2>Influenza cases in ISU FLUture with 96% or greater similarity to query sequence</h2> 
         <div class="chartChild"> 
                 <h3>State of Detection</h3> 
                 <div id="stateChart" class="chartChild"></div> 
@@ -149,7 +168,7 @@ echo <<<EOF
 </div>
 EOF;
 
-echo count($state) . " sequences above 98% identity threshold<br/>";
+echo count($state) . " sequences above 96% identity threshold<br/>";
 echo $table;
 
 echo "<script>";
