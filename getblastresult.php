d90f87dd948d3fd9f0fe3181183d4da4589ee233
anugrah-saxena <anugrahsaxenamnit@gmail.com>
2019-08-16 11:11:05 -0500

    UPDATED 1. Added checks for the incoming sequences to validate them before running blast tool on them.

--- getblastresult.php
+++ getblastresult.php
@@ -39,6 +39,16 @@ $cladeArray = json_decode(file_get_contents("./js/orders.json"), true);
 $seq_input = $_POST['seq'];
 $blast_type = $_POST['blast'];
 
+#Remove ndels if present in the sequence input
+$seq_input = str_replace("-","", $seq_input);
+
+#Verify sequence input
+if(!preg_match('/^[atgcrykmswbdhvn]+$/' , strtolower($seq_input)) && !preg_match('/^[knimrst*ylfcwedvgaqhp]+$/' , strtolower($seq_input)))
+{
+        echo "<p>The input sequence consists of invalid characters.</p>";
+        return;
+}
+
 $temp = tmpfile();
 fwrite($temp, $seq_input);
 fseek($temp, 0);
